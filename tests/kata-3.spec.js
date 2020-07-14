25f6dedfb1fdd5d1329c117c8b473c6b6bac5f00
Tony Medrano <info@tonymedrano.com>
2020-07-14 11:51:58 +0200

    feat(magali-tech): added more components

--- tests/kata-3.spec.js
+++ tests/kata-3.spec.js
@@ -3,29 +3,23 @@ import { kata3 } from "../src/kata-3";
 
 describe("kata-3", function() {
     it("n=1 m=250 should return valid values", () => {
-        setTimeout(() => {
-            assert.deepEqual(kata3(1, 250), [
-                [1, 1],
-                [42, 2500],
-                [246, 84100]
-            ]);
-        }, 100);
+        assert.deepEqual(kata3(1, 250), [
+            [1, 1],
+            [42, 2500],
+            [246, 84100]
+        ]);
     });
 
     it("n=41 m=250 should return valid values", () => {
-        setTimeout(() => {
-            assert.deepEqual(kata3(41, 250), [
-                [42, 2500],
-                [246, 84100],
-            ]);
-        }, 100);
+        assert.deepEqual(kata3(41, 250), [
+            [42, 2500],
+            [246, 84100],
+        ]);
     });
 
     it("n=10 m=42 should return valid values", () => {
-        setTimeout(() => {
-            assert.deepEqual(kata3(10, 42), [
-                [42, 2500]
-            ]);
-        }, 100);
+        assert.deepEqual(kata3(10, 42), [
+            [42, 2500]
+        ]);
     });
 });
